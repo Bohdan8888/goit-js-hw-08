7c934a95f9c70fb9e9a85fb4075bfcd30b3cc11a
Bohdan <bogdaansharyga@gmail.com>
2024-01-19 10:41:39 +0200

    redact

--- js/gallery.js
+++ js/gallery.js
@@ -66,28 +66,35 @@ const images = [
 
 const gallery = document.querySelector(".gallery");
 let lightbox;
+
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
+
   if (event.target.classList.contains("gallery-image")) {
     const originalSrc = event.target.dataset.source;
+
     lightbox = basicLightbox.create(
-      `<img width="1400" height="900" src="${originalSrc}">`
+      `<img width="1400" height="900" src="${originalSrc}">`,
+      {
+        onShow: () => {
+         
+          document.addEventListener("keydown", handleKeyDown);
+        },
+        onClose: () => {
+         
+          document.removeEventListener("keydown", handleKeyDown);
+        },
+      }
     );
+
     lightbox.show();
-    document.addEventListener("keydown", handleKeyDown);
   }
 });
 
 function handleKeyDown(event) {
   if (event.key === "Escape" || event.code === "Escape") {
-    closeLightbox();
-  }
-}
 
-function closeLightbox() {
-  if (lightbox && lightbox.visible()) {
     lightbox.close();
-    document.removeEventListener("keydown", handleKeyDown);
   }
 }
 
